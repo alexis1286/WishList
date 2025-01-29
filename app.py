6713753a5bd3172c754545863db302e6ad5ea810
Alexis <alovitzschool@gmail.com>
2025-01-29 16:52:43 -0500

    cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import json
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+CORS(app)  # Allow requests from any frontend
 # Load wishlist from file
 def load_wishlist():
     try:
